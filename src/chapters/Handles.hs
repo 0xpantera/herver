d9eb111371b754f9f6cb1d34208105d16dfef0a6
Francososa <franky.sosa@gmail.com>
2020-11-05 12:38:17 -0600

    Playing with file handles

---
+++ src/chapters/Handles.hs
@@ -0,0 +1,27 @@
+module Chunks where
+
+import Control.Exception.Safe (bracket)
+import System.IO ( hClose, openFile, hPutStrLn, withFile, IOMode (..) )
+
+
+
+main :: IO ()
+main = do
+  h <- openFile "greeting.txt" WriteMode
+  hPutStrLn h "hello"
+  hPutStrLn h "world"
+  hClose h
+
+main2 :: IO ()
+main2 = 
+    bracket (openFile "greetings.txt" WriteMode) hClose $ \h ->
+        do
+            hPutStrLn h "hello"
+            hPutStrLn h "world"
+
+main3 :: IO ()
+main3 =
+    withFile "greetings.txt" WriteMode $ \h ->
+        do
+            hPutStrLn h "hello"
+            hPutStrLn h "world"
