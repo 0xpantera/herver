c3c28c649f2038c389e9321c2a064dd2718824e6
Francososa <franky.sosa@gmail.com>
2020-11-05 20:02:58 -0600

    Use unless from Control.Monad to simplify forChunks_

--- src/chapters/Chunks.hs
+++ src/chapters/Chunks.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE TypeApplications #-}
 module Chunks where
 
+import Control.Monad
 import System.IO
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
@@ -19,19 +20,21 @@ printCapitalizedText =
 forChunks_ ::
   IO chunk              -- ^ Producer of chunks
   -> (chunk -> Bool)    -- ^ Does chunk indicate EOF?
-  -> (chunk -> IO x)    -- ^ What to do with each chunk  
+  -> (chunk -> IO ())    -- ^ What to do with each chunk  
   -> IO ()
 forChunks_ getChunk isEnd f = continue
   where
     continue =
       do
         chunk <- getChunk
-        case (isEnd chunk) of
+        unless (isEnd chunk) (f chunk)
+        
+        {--case (isEnd chunk) of
           True -> return ()
           False ->
             do
               _ <- f chunk
-              continue
+              continue--}
 
 digitsOnly :: T.Text -> T.Text
 digitsOnly s = T.pack $ filter isDigit $ T.unpack s
@@ -44,3 +47,6 @@ capitalizeLast s = (T.init s) <> upperLast s
   where
     upperLast = (T.toUpper . T.singleton . T.last)
 
+-- TODO
+--unParen :: T.Text -> Maybe T.Text
+--unparen = _
